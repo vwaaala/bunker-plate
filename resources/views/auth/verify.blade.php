3578396d89ee3600f01b784ccbeacc0d5bbbe9d8
Md Rafiql Islam <vwaaala@gmail.com>
2024-09-27 18:03:18 +0600

    cosmetic changes

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row justify-content-center">
+    <div class="row justify-content-center mt-5">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Verify Your Email Address') }}</div>
